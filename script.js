412fd18165790213d1b02f8dc007416fd0562e0a
Kathleen Hamrick <kchamrick@gmail.com>
2024-12-11 16:58:42 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Step 1: Display introductory message
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+/*Step 2: Use three different arithmetic operators to arrive at the three combination numbers specified (10, 40, 39) and ensure that each calculation results in the required corresponding combination number. */
+// Step 3: Generate the combination using calculations
+let code1 = 20-10; // first number is 10
+let code2 = 20 * 2; // second number is 40
+let code3 = 78 / 2; // third number is 39
+let fullMessage = `${message}\nCombination: ${code1} - ${code2} - ${code3}`;
+console.log("Kathleen");
+console.log(code1);
+console.log(code2);
+console.log(code3);
+console.log(fullMessage);
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10-40-39!");
